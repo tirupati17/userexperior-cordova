348061f6053c177f9c1c1c12c67ed24cdd6753f2
Tirupati Balan <tirupati@tirupatibalan.com>
2019-06-17 21:32:38 +0530

    cordova remaining function

--- userexperior-cordova-plugin/src/ios/CDVUserExperior.h
+++ userexperior-cordova-plugin/src/ios/CDVUserExperior.h
@@ -3,5 +3,10 @@
 @interface CDVUserExperior : CDVPlugin
 
 - (void)startRecording:(CDVInvokedUrlCommand*)command;
+- (void)setCustomTag:(CDVInvokedUrlCommand*)command;
+- (void)setUserIdentifier:(CDVInvokedUrlCommand*)command;
+- (void)resumeRecording:(CDVInvokedUrlCommand*)command;
+- (void)pauseRecording:(CDVInvokedUrlCommand*)command;
+- (void)stopRecording:(CDVInvokedUrlCommand*)command;
 
 @end
--- userexperior-cordova-plugin/src/ios/CDVUserExperior.m
+++ userexperior-cordova-plugin/src/ios/CDVUserExperior.m
@@ -18,5 +18,51 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+- (void)setCustomTag:(CDVInvokedUrlCommand*)command {
+    CDVPluginResult *pluginResult = nil;
+    NSString *tag = command.arguments[0];
+    NSString *type = command.arguments[1];
+
+    if (tag.length>0 && type.length>0) {
+        [UserExperior setCustomTag:tag customType:type];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)setUserIdentifier:(CDVInvokedUrlCommand*)command {
+    CDVPluginResult* pluginResult = nil;
+    NSString *identifier = command.arguments[0];
+
+    if (identifier.length > 0) {
+        [UserExperior setUserIdentifier:identifier];
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)resumeRecording:(CDVInvokedUrlCommand*)command {
+    [UserExperior resumeRecording];
+
+    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+}
+
+- (void)pauseRecording:(CDVInvokedUrlCommand*)command {
+    [UserExperior pauseRecording];
+
+    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+}
+
+- (void)stopRecording:(CDVInvokedUrlCommand*)command {
+    [UserExperior stopRecording];
+
+    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
+}
+
 @end
 
